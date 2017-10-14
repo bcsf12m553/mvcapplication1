50a72503301e99eb2c57ba4650403d982973b942
bcsf12m553 <bcsf12m553@pucit.edu.pk>
2017-10-14 17:43:59 +0500

    first push

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+using System.Data.Entity;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        soundsystemsEntities mdi = new soundsystemsEntities();
+
+
+        public ActionResult Index()
+        {
+
+
+            return View(mdi.Products.ToList());
+        }
+
+    }
+}
